4f1452b96a91ce038a3bfdc679239d2275d1ad90
kacper0276 <kacper0276@op.pl>
2023-11-12 09:43:51 +0100

    Dodaj pliki projektów.

---
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/Namespace/Program2.cs
@@ -0,0 +1,15 @@
+﻿namespace PierwszaAplikacjaKonsolowa.Namespace;
+
+internal class Program2
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello world!");
+}
+}
+
+class Car
+{
+
+}
+
