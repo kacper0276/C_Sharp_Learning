89c029fa795e89197d9146b745322a12e4d19186
kacper0276 <kacper0276@op.pl>
2023-11-13 08:47:11 +0100

    Typy Referencyjne

--- PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/Program.cs
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int character = Console.Read(); // Pobiera tylko pierwszy znak (jako int)
+Console.WriteLine(character);
+string napis = Console.ReadLine();
+Console.WriteLine(napis);
+// PascalCase - NapisZmiennaPrzyklad
+// Cummelcase - napisZmiennaPrzyklad - głównie zmienne lokalne i globalne
+// UpperCase  - PI_VALUE - głównie do stałych
+string? yearString = Console.ReadLine();
+bool isValid = int.TryParse(yearString, out int year);
+Console.WriteLine(isValid);
+Console.WriteLine(year);
+
+int firstNumber = 20;
+int secondNumber = firstNumber;
---
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/Zmienne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierwszaAplikacjaKonsolowa
+{
+    internal class Zmienne
+    {
+        // Zmienne
+        int a = 0;
+        float flo = 3.13f;
+        double d = 2.5;
+        bool isValidate = true;
+        char ad = 'a';
+        enum Type
+        {
+            LOW,
+            HIGH,
+            MEDIUM
+        };
+
+        struct Price
+        {
+            public int ValueDecimal;
+            public int ValuePointer;
+
+            public Price(int valueDevcimal, int valuePointer)
+            {
+                this.ValueDecimal = valueDevcimal;
+                this.ValuePointer = valuePointer;
+            }
+        };
+
+        Price price = new Price(1, 2);
+    }
+}
