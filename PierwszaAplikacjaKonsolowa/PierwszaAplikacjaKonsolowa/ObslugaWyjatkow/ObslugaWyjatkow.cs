258ac200f5b72d95e29f0e3c0c1769ef7d152c8d
kacper0276 <kacper0276@op.pl>
2023-11-14 19:13:59 +0100

    Programowanie Obiektowe - Część 1

---
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/ObslugaWyjatkow/ObslugaWyjatkow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierwszaAplikacjaKonsolowa.ObslugaWyjatkow
+{
+    public class ObslugaWyjatkow
+    {
+        public ObslugaWyjatkow()
+        {
+            static void ThrowException(int id)
+            {
+                if (id == 0)
+                {
+                    throw new ArgumentNullException("id");
+                }
+            }
+
+            try
+            {
+                ThrowException(1);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+                //    throw; <- nie stracimy stosu wywołań
+            }
+            catch (InvalidOperationException e) // Nie obsługiwany wyjątek
+            {
+                // StackTrace - wyrzuca w której linicje jest wyjątek
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally zawsze się wykona");
+            }
+
+            try
+            {
+                ThrowException(1);
+            }
+            finally
+            {
+                Console.WriteLine("Nie obsługiwane żadne wyjątki");
+            }
+        }
+    }
+}
--- PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/ZakresyWidocznosci/ZakresyWidocznosci.cs
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/ZakresyWidocznosci/ZakresyWidocznosci.cs
@@ -8,9 +8,38 @@ namespace PierwszaAplikacjaKonsolowa.ZakresyWidocznosci
 {
     public class ZakresyWidocznosci
     {
+        public string Name { get; set; }
+        private int Id { get; set; }
+        protected int priority { get; set; } = 0; // Współdzielenie między klasami które dziedziczą
+         // internal -  Dostęp do zmiennej - dostępna tylko w jednym projekcie
+
+        public void SetId(int id)
+        {
+            if(id < 0)
+            {
+                Id = 1000;
+                return;
+            }
+
+            Id = id;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
         public ZakresyWidocznosci()
         {
             
+        }
+    }
+
+    public class Item2 : ZakresyWidocznosci
+    {
+        public Item2()
+        {
+            priority = 2;
         }
     }
 }
