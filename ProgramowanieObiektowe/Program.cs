3c5ae31e054b1a752a3a4f595dc56817c5ad108a
kacper0276 <kacper0276@op.pl>
2023-11-16 09:42:32 +0100

    Programowanie Obiektowe

---
+++ ProgramowanieObiektowe/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProgramowanieObiektowe.TworzenieObiektow;
+
+Price price = new Price(15);
+
+Price price1 = Price.Create(10);
---
+++ ProgramowanieObiektowe/TworzenieObiektow/Price.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektowe.TworzenieObiektow
+{
+    public class Price
+    {
+        public decimal Amount { get; }
+
+        public Price(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public Price()
+        {
+            
+        }
+
+        public static Price Create(decimal amount)
+        {
+            return new Price(amount);
+        }
+
+        public string GetValue(string pattern)
+        {
+            return string.Format(pattern, Amount);
+        }
+
+        public string GetValue()
+        {
+            return Amount.ToString();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Price);
+        }
+
+        public bool Equals(Price other)
+        {
+            if(ReferenceEquals(other, null)) return false;
+            if(ReferenceEquals(other, this)) return true;
+
+            return this.Amount == other.Amount;
+        }
+    }
+}
