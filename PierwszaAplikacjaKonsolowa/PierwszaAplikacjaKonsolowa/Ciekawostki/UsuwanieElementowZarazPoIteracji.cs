b4da9d81be114b77653f8a09b470594b81fcf1ef
kacper0276 <kacper0276@op.pl>
2023-11-22 16:11:10 +0100

    Ciekawe kody - Usuwanie zaraz po iteracji z listy

---
+++ PierwszaAplikacjaKonsolowa/PierwszaAplikacjaKonsolowa/Ciekawostki/UsuwanieElementowZarazPoIteracji.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierwszaAplikacjaKonsolowa.Ciekawostki
+{
+    public class UsuwanieElementowZarazPoIteracji
+    {
+        public UsuwanieElementowZarazPoIteracji()
+        {
+            List<int> a = new();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+            a.Add(4);
+            a.Add(5);
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+                Console.WriteLine(i);
+                // jak --i, najpierw i = -1, outOfRange, a i-- najpierw usunie przy 0, a potem i = -1
+                a.RemoveAt(i--); // Najpierw usuwa przy indexie i = 0, potem zmiana na -1, inkrementacja i = 0, i tak ciągle
+                Console.WriteLine(i);
+                Console.WriteLine("----------------------------");
+            }
+
+
+        }
+    }
+}
