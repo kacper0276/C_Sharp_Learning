70d3502bbb059a96924c0356ae5e313014f02b53
kacper0276 <kacper0276@op.pl>
2023-11-23 12:27:18 +0100

    Delegaty, Anonimowe funkcje, Lamda - następny odcinek: SOLID

--- ProgramowanieObiektowe/DelegatyAnonimoweFunkcjeLambda/DelegatyAnonimoweFunkcjeLambda.cs
+++ ProgramowanieObiektowe/DelegatyAnonimoweFunkcjeLambda/DelegatyAnonimoweFunkcjeLambda.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProgramowanieObiektowe.DelegatyAnonimoweFunkcjeLambda
 {
+    delegate string ChangeText(string text);
+
     public class DelegatyAnonimoweFunkcjeLambda
     {
         private static string text = "abc";
@@ -19,6 +21,49 @@ namespace ProgramowanieObiektowe.DelegatyAnonimoweFunkcjeLambda
         public static string GetText()
         {
             return text;
+        }
+
+        // Gotowe delegaty func, action
+        // Action - służy do zapisania metody która nic nie zwraca
+        // Func - działa jak zwykła delegata
+
+        public static void Main(string[] args)
+        {
+            ChangeText changeText = new ChangeText(AddText);
+            changeText(" abc123");
+            Console.WriteLine("Wartość tekstu: {0}", GetText());
+            changeText += changeText; // Dwa razy wykona tą samą metodę
+            changeText("dwa");
+            AddText(" test 123");
+            Console.WriteLine("Wartość tekstu: {0}", GetText());
+            Func<string, string> func = delegate (string t) { text += t; return text; };  // <co przyjmuje, co zwraca>
+            func(" From Func ");
+            Console.WriteLine("Wartość tekstu: {0}", GetText());
+            Func<string, string> func1 = AddText;
+            func1(" Func1 ");
+            Console.WriteLine("Wartość tekstu: {0}", GetText());
+
+            // Funkcja anonimowa (nie istnieją na poziomie klas)
+            Func<int, int, int> func2 = (int a, int b) => a + b;
+
+            ShowResult(Add(1, 2));
+            ShowResult(func2(1, 2));
+
+            // Action - nic nie zwraca
+            Action<int> action = delegate (int result) { Console.WriteLine(result); };
+            action(Add(3, 4));
+            Action<int> action1 = (int result) => Console.WriteLine(result);
+            action1(Add(4, 5));
+
+            Console.ReadKey();
+        }
+
+        // Wyrażenie Lambda
+        public static void ShowResult(int result) => Console.WriteLine(result);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
