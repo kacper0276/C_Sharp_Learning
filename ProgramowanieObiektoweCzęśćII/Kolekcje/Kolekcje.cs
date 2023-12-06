ad5617a04b25b7e1cfe92a67943da839158bad5b
kacper0276 <kacper0276@op.pl>
2023-12-06 14:08:54 +0100

    Kolekcje - Następny odcinek: LINQ

---
+++ ProgramowanieObiektoweCzęśćII/Kolekcje/Kolekcje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweCzęśćII.Kolekcje
+{
+    internal class Kolekcje
+    {
+        public Kolekcje()
+        {
+            
+        }
+    }
+}
