bf7a85c1f44588d955b7afbad8652bbd51970a02
kacper0276 <kacper0276@op.pl>
2023-12-06 22:21:40 +0100

    LINQ - Następny odcinek Wzorce projektowe

---
+++ ProgramowanieObiektoweCzęśćII/LINQ/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweCzęśćII.LINQ
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = nameof(Item);
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
