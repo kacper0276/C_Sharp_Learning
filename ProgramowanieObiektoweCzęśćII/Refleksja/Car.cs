546e0626ff7859465e36e2d840e4aca391834d51
kacper0276 <kacper0276@op.pl>
2023-12-08 10:47:36 +0100

    Refleksja - następny odcinek: Dynamiczne tworzenie obiektów

---
+++ ProgramowanieObiektoweCzęśćII/Refleksja/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweCzęśćII.Refleksja
+{
+    public class Car
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; private set; } = nameof(Car);
+        public bool IsRunning { get; private set; } = false;
+
+        private Car() { }
+
+        public static Car Create(string name)
+        {
+            return new Car { Name = name };
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+        }
+
+        public void RunCar()
+        {
+            IsRunning = true;
+        }
+
+        public void StopCar()
+        {
+            IsRunning = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {IsRunning}";
+        }
+    }
+}
---
+++ ProgramowanieObiektoweCzęśćII/Refleksja/Refleksja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweCzęśćII.Refleksja
+{
+    class Refleksja
+    {
+        public Refleksja()
+        {
+            // Refleksja to zbiór metod z system. , umożliwia dynamiczne tworzenie zmiennych, klas i obiektów reflection
+            var car = Car.Create("Porshe");
+            var type = car.GetType();
+            Console.WriteLine($"Car {type.Name}"); // - typ zmiennej
+
+            var methods = type.GetMethods();
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.Name); // Wszystkie metody
+            }
+            car.ChangeName("Tester");
+            var changeNameMethod = type.GetMethod(nameof(Car.ChangeName));
+            changeNameMethod!.Invoke(car, new object[] { "BMW" });
+            Console.WriteLine(car);
+            var runCarMethod = type.GetMethod(nameof(Car.RunCar));
+            runCarMethod!.Invoke(car, null);
+            Console.WriteLine(car);
+            var idField = type.GetField($"<{nameof(Car.Id)}>k__BackingField", BindingFlags.NonPublic | BindingFlags.Instance);
+            idField!.SetValue(car, Guid.NewGuid());
+            Console.WriteLine(car);
+        }
+    }
+}
