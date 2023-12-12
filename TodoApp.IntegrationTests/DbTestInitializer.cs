ede87a123f9489ed7a29111fcfccc9c123419217
kacper0276 <kacper0276@op.pl>
2023-12-12 15:11:02 +0100

    Poprawa testów bazy danych

---
+++ TodoApp.IntegrationTests/DbTestInitializer.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApp.Infrastructure;
+using TodoApp.Infrastructure.Database;
+
+namespace TodoApp.IntegrationTests
+{
+    internal sealed class DbTestInitializer : IDbInitializer
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbTestInitializer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Start()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var databaseOptions = scope.ServiceProvider.GetRequiredService<DatabaseOptions>();
+
+            if (!databaseOptions.AllowMigrations)
+            {
+                return;
+            }
+
+            var migrationRunner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+            migrationRunner.MigrateUp();
+        }
+    }
+}
--- TodoApp.IntegrationTests/QuestFixture.cs
+++ TodoApp.IntegrationTests/QuestFixture.cs
@@ -1,18 +1,51 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using System.Text.Json;
 using TodoApp.Core;
 using TodoApp.Infrastructure;
+using TodoApp.Infrastructure.Database;
 
 namespace TodoApp.IntegrationTests
 {
-    public class QuestFixture
+    public class QuestFixture : IDisposable
     {
         public IServiceProvider ServiceProvider { get; set; }
 
         public QuestFixture()
         {
             ServiceProvider = new ServiceCollection().AddCore()
-                                                     .AddInfrastructure("Host=localhost;Database=todoappdatabase;Username=root;Password=")
-                                                     .BuildServiceProvider();
+                                .AddInfrastructure(GetAppsettings())
+                                .BuildServiceProvider()
+                                .UseInfrastructure();
+        }
+
+        private Dictionary<string, object> GetAppsettings()
+        {
+            var appFolder = AppContext.BaseDirectory
+                ?? throw new InvalidOperationException("Cannot get Application folder directory");
+            using FileStream fileStream = File.Open(appFolder + Path.DirectorySeparatorChar + "appsettings.json", FileMode.Open, FileAccess.Read);
+            var settings = JsonSerializer.Deserialize<Dictionary<string, object>>(fileStream)
+                    ?? throw new InvalidOperationException("Cannot deserialize 'appsettings.json', please ensure that this file exists");
+            return settings;
+        }
+
+        public void Dispose()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var connection = scope.ServiceProvider.GetRequiredService<IDbConnection>();
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"DROP DATABASE IF EXISTS {connection.Database}";
+            command.ExecuteNonQuery();
+            command.Dispose();
+            connection.Close();
+            connection.Dispose();
+            scope.Dispose();
+            if (ServiceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+            GC.SuppressFinalize(this);
         }
     }
 }
