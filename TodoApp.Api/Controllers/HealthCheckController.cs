35f7c10bc6f32946430daf7275cb5484aba57f56
kacper0276 <kacper0276@op.pl>
2023-12-15 13:01:44 +0100

    Kontroler - Następny odcinek: Minimal API

---
+++ TodoApp.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            await Task.CompletedTask;
+            return Ok("TodoApp");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            await Task.CompletedTask;
+            return Ok($"Added! {id}");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            await Task.CompletedTask;
+            return CreatedAtAction(nameof(Get), new { id = 1 }, null); // , null - dodanie url w nagłówku
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id) 
+        {
+            await Task.CompletedTask;
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Task.CompletedTask;
+            return NoContent();
+        }
+    }
+}
