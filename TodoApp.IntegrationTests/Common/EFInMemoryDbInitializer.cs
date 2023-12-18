960b9d15e8361f7f0b62489f19c21d1abcb65bee
kacper0276 <kacper0276@op.pl>
2023-12-18 18:18:17 +0100

    Test integracyjny - Następny odcinek: Testowanie różnych przypadków

---
+++ TodoApp.IntegrationTests/Common/EFInMemoryDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using TodoApp.Infrastructure.Database;
+
+namespace TodoApp.IntegrationTests.Common
+{
+    internal sealed class EFInMemoryDbInitializer : IDbInitializer
+    {
+        private readonly TodoDbContext _todoDbContext;
+
+        public EFInMemoryDbInitializer(TodoDbContext todoDbContext)
+        {
+            _todoDbContext = todoDbContext;
+        }
+
+        public async Task Start()
+        {
+            await _todoDbContext.Database.EnsureCreatedAsync();
+        }
+    }
+}
--- TodoApp.IntegrationTests/Common/TestApplicationFactory.cs
+++ TodoApp.IntegrationTests/Common/TestApplicationFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApp.Infrastructure.Database;
 
@@ -7,15 +8,9 @@ namespace TodoApp.IntegrationTests.Common
 {
     public sealed class TestApplicationFactory : WebApplicationFactory<Program>
     {
-        public HttpClient Client { get; }
-
-        public TestApplicationFactory()
-        {
-            Client = CreateClient();
-        }
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureServices(SetupEFCoreInMemory);
             builder.UseEnvironment("test");
         }
 
@@ -27,6 +22,25 @@ namespace TodoApp.IntegrationTests.Common
             await context.DisposeAsync();
             scope.Dispose();
             await base.DisposeAsync();
+        }
+
+        private static void SetupEFCoreInMemory(IServiceCollection services)
+        {
+            // remove implementation of infrastructure dbContext
+            var contextDb = services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(TodoDbContext));
+            if (contextDb != null)
+            {
+                services.Remove(contextDb);
+                var options = services.Where(r => (r.ServiceType == typeof(DbContextOptions))
+                  || (r.ServiceType.IsGenericType && r.ServiceType.GetGenericTypeDefinition() == typeof(DbContextOptions<>))).ToList();
+
+                foreach (var option in options)
+                {
+                    services.Remove(option);
+                }
+            }
+            services.AddDbContext<TodoDbContext>(options => options.UseInMemoryDatabase("TodoDbApp"));
+            services.AddScoped<IDbInitializer, EFInMemoryDbInitializer>();
         }
     }
 }
