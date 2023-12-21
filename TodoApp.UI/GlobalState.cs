8a55ae5227f4ffe38f83db70bad08d210a9ea825
kacper0276 <kacper0276@op.pl>
2023-12-21 09:17:55 +0100

    Komunikacja między komponentami - Następny odcinek: Formularze i walidacja

---
+++ TodoApp.UI/GlobalState.cs
@@ -0,0 +1,23 @@
+﻿namespace TodoApp.UI
+{
+    public class GlobalState
+    {
+        public event Action OnChange = default!;
+        public readonly List<string> MessegesFirst = new();
+        public readonly List<string> MessagesSecond = new();
+
+        public void SendMessageToFirstChat(string message)
+        {
+            MessegesFirst.Add(message);
+            NotifyStateChanged();
+        }
+
+        public void SendMessageToSecondChat(string message)
+        {
+            MessagesSecond.Add(message);
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
